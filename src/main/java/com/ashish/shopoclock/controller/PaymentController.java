6c7d9b07d9c32f30305a3395e679154a7e42bbe8
ashishtomar <atomar300@gmail.com>
2024-01-24 01:53:32 -0500

    final update done

--- src/main/java/com/ashish/shopoclock/controller/PaymentController.java
+++ src/main/java/com/ashish/shopoclock/controller/PaymentController.java
@@ -14,6 +14,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/v1")
 public class PaymentController {
 
